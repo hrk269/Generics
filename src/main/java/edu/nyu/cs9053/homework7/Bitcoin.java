acaef49ad0e3a2da70a2d848cf47ea238d4b1497
hrk269 <35784897+hrk269@users.noreply.github.com>
2018-04-04 13:38:09 -0400

    4 space indents

--- src/main/java/edu/nyu/cs9053/homework7/Bitcoin.java
+++ src/main/java/edu/nyu/cs9053/homework7/Bitcoin.java
@@ -2,32 +2,32 @@ package edu.nyu.cs9053.homework7;
 
 public class Bitcoin implements Cryptocurrency{
 
-	private final double amount;
+    private final double amount;
 
-	public Bitcoin(double amount) {
-		this.amount = amount;
-	}
+    public Bitcoin(double amount) {
+        this.amount = amount;
+    }
 
-	@Override public double getAmount(){
-		return this.amount;
-	}
+    @Override public double getAmount(){
+        return this.amount;
+    }
 
-	@Override public boolean equals(Object obj) {
-		if (this == obj) {
-			return true;
-		}
-		if ( (obj == null) || (getClass() != obj.getClass()) ) {
-			return false;
-		}
-		Bitcoin cryptoCurrency = (Bitcoin) obj;
-		if (getAmount() != cryptoCurrency.getAmount()) {
-			return false;
-		}
-		return true;
-	}
+    @Override public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if ( (obj == null) || (getClass() != obj.getClass()) ) {
+            return false;
+        }
+        Bitcoin cryptoCurrency = (Bitcoin) obj;
+        if (getAmount() != cryptoCurrency.getAmount()) {
+            return false;
+        }
+        return true;
+    }
 
-	@Override public int hashCode() {
-		Double result = (Double) getAmount();
+    @Override public int hashCode() {
+        Double result = (Double) getAmount();
         return result.hashCode();
-	}
+    }
 }
