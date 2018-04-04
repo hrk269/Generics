aaaaf1a9d091320d0987518708f4b129e0985cc2
hrk269 <35784897+hrk269@users.noreply.github.com>
2018-04-04 13:39:35 -0400

    4 space indents

--- src/main/java/edu/nyu/cs9053/homework7/CryptoWalletTransfer.java
+++ src/main/java/edu/nyu/cs9053/homework7/CryptoWalletTransfer.java
@@ -2,9 +2,9 @@ package edu.nyu.cs9053.homework7;
 
 public class CryptoWalletTransfer<T extends CryptoWallet> {
 	
-	public void walletTransfer(Wallet<? extends T> producerCryptoWallet, Wallet<? super T> consumerCryptoWallet){
-		for (int i = 0; i < producerCryptoWallet.size(); i++) {
-			consumerCryptoWallet.add(producerCryptoWallet.get(i));
-		}
-	}
+    public void walletTransfer(Wallet<? extends T> producerCryptoWallet, Wallet<? super T> consumerCryptoWallet){
+        for (int i = 0; i < producerCryptoWallet.size(); i++) {
+            consumerCryptoWallet.add(producerCryptoWallet.get(i));
+        }
+    }
 }
