ed6b37033d9c5db6412b68b7cdae35c24453ee28
hrk269 <35784897+hrk269@users.noreply.github.com>
2018-04-04 13:38:46 -0400

    4 space indents

--- src/main/java/edu/nyu/cs9053/homework7/CryptoWallet.java
+++ src/main/java/edu/nyu/cs9053/homework7/CryptoWallet.java
@@ -2,7 +2,7 @@ package edu.nyu.cs9053.homework7;
 
 public class CryptoWallet<T extends Cryptocurrency> extends Wallet{
 
-	public CryptoWallet() {
-		super();
-	}
+    public CryptoWallet() {
+        super();
+    }
 }
